46efe1f1be9320874f94621ebe162d028b995ce0
karthikeyan-T12 <t.karthikeyan.0712@gmail.com>
2026-04-13 19:19:09 +0530

    Add files via upload

---
+++ minoperation.java
@@ -0,0 +1,26 @@
+package com.leetcode.pratice;
+
+class minopp
+{
+    public int operation(int[] nums,int k)
+    {
+        int sum = 0;
+        for(int n:nums)
+        {
+            sum+=n;
+        }
+        return sum%k;
+    }
+}
+
+public class minoperation
+{
+    public static void main(String[] args)
+    {
+        minopp o1 = new minopp();
+        int nu[]={3,9,7};
+        int result= o1.operation(nu,5);
+        System.out.println(result);
+    }
+
+}
