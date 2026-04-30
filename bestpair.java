8eac96b2f93a99b15d150301008cab0df406893e
karthikeyan-T12 <karthikeyan.0712@gmail.com>
2026-04-30 21:58:17 +0530

    new problem was solve

---
+++ bestpair.java
@@ -0,0 +1,20 @@
+package Leetcode;
+public class bestpair
+{
+    public static void main(String args[])
+    {
+        int nums[] = {1,2,3};
+        int pairs=0;
+        for(int i=0; i<nums.length; i++)
+        {
+            for(int j=i+1; j< nums.length; j++)
+            {
+                if(nums[i]==nums[j])
+                {
+                    pairs++;
+                }
+            }
+        }
+        System.out.println(pairs);
+    }
+}
